f864331ee4f29c9e72c60bab698ed21e4aef0e3a
wakanda0987645678 <wakanda098765@proton.me>
2025-11-05 02:01:02 +0000

    b-4four

--- src/components/others/icons/font-awesome-icon.js
+++ src/components/others/icons/font-awesome-icon.js
@@ -1,15 +1,59 @@
-import React, { Fragment } from 'react'
-import { string } from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import {
+  FaUser,
+  FaHome,
+  FaCompass,
+  FaBell,
+  FaEnvelope,
+  FaBookmark,
+  FaImages,
+  FaHeart,
+  FaUsers,
+  FaStar,
+  FaUserEdit,
+  FaCog,
+  FaSignOutAlt,
+  FaShieldAlt,
+  FaQuestionCircle,
+} from 'react-icons/fa'
 
-const FAIcon = ({ icon, ...props }) => (
-  <Fragment>
-    <i className={classNames('fas', `fa-${icon}`)} {...props} />
-  </Fragment>
-)
+// Map the icon name strings used across the app to react-icons components
+const ICONS = {
+  user: FaUser,
+  home: FaHome,
+  compass: FaCompass,
+  bell: FaBell,
+  envelope: FaEnvelope,
+  bookmark: FaBookmark,
+  images: FaImages,
+  heart: FaHeart,
+  users: FaUsers,
+  star: FaStar,
+  'user-edit': FaUserEdit,
+  cog: FaCog,
+  'sign-out-alt': FaSignOutAlt,
+  'shield-alt': FaShieldAlt,
+}
+
+const FAIcon = ({ icon, className, size = 18, style = {}, ...props }) => {
+  const Icon = ICONS[icon] || FaQuestionCircle
+  return (
+    <Icon
+      className={classNames('fa-icon', className)}
+      size={size}
+      style={style}
+      {...props}
+    />
+  )
+}
 
 FAIcon.propTypes = {
-  icon: string.isRequired,
+  icon: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  style: PropTypes.object,
 }
 
 export default FAIcon
